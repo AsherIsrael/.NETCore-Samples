f71807cb560d38d7b815bb5bd2ff107529f25913
Asher Israel <artificialisrael@yahoo.com>
2016-08-17 19:04:46 -0700

    slight reformatting, nancy version started

--- GreatNumberGame/Controllers/NumberController.cs
+++ GreatNumberGame/Controllers/NumberController.cs
@@ -1,46 +1,46 @@
-using System;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+// using System;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Http;
 
-namespace GreatNumberGame.Controllers{
-    public class NumberController : Controller{
+// namespace GreatNumberGame.Controllers{
+//     public class NumberController : Controller{
 
-        [HttpGet]
-        public IActionResult index(string result){
+//         [HttpGet]
+//         public IActionResult index(string result){
 
-            if(HttpContext.Session.GetInt32("TheNumber") == null){
-                HttpContext.Session.SetInt32("TheNumber", (new Random().Next(1, 100)));
-            }
+//             if(HttpContext.Session.GetInt32("TheNumber") == null){
+//                 HttpContext.Session.SetInt32("TheNumber", (new Random().Next(1, 100)));
+//             }
 
-            if(result == null){
-                ViewData["LastGuess"] = "";
-            }else{
-                ViewData["LastGuess"] = result;
-            }
+//             if(result == null){
+//                 ViewData["LastGuess"] = "";
+//             }else{
+//                 ViewData["LastGuess"] = result;
+//             }
 
-            ViewData["TheNumber"] = HttpContext.Session.GetInt32("TheNumber");
-            return View("index");
-        }
+//             ViewData["TheNumber"] = HttpContext.Session.GetInt32("TheNumber");
+//             return View("index");
+//         }
 
-        [HttpPost]
-        public IActionResult guess(int Number){
+//         [HttpPost]
+//         public IActionResult guess(int Number){
 
-            string result;
-            if(Number > HttpContext.Session.GetInt32("TheNumber")){
-                result = "Too High";
-            }else if(Number < HttpContext.Session.GetInt32("TheNumber")){
-                result = "Too Low";
-            }else{
-                result = "You Got It!";
-            }
+//             string result;
+//             if(Number > HttpContext.Session.GetInt32("TheNumber")){
+//                 result = "Too High";
+//             }else if(Number < HttpContext.Session.GetInt32("TheNumber")){
+//                 result = "Too Low";
+//             }else{
+//                 result = "You Got It!";
+//             }
             
-            return index(result);
-        }
+//             return index(result);
+//         }
 
-        [HttpGet]
-        public IActionResult reset(){
-            HttpContext.Session.Clear();
-            return index(null);
-        }
-    }
-}
+//         [HttpGet]
+//         public IActionResult reset(){
+//             HttpContext.Session.Clear();
+//             return index(null);
+//         }
+//     }
+// }
--- GreatNumberGame/Program.cs
+++ GreatNumberGame/Program.cs
@@ -1,12 +1,17 @@
 using System;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
-namespace GreatNumberGame{
+namespace GreatNumberGame
+{
 
-    public class Program{
+    public class Program
+    {
 
-        public static void Main(string[] args){
+        public static void Main(string[] args)
+        {
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -14,6 +19,60 @@ namespace GreatNumberGame{
                 .Build();
 
             host.Run();
+        }
+    }
+
+    public class NumberController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult index(string result)
+        {
+
+            if(HttpContext.Session.GetInt32("TheNumber") == null)
+            {
+                HttpContext.Session.SetInt32("TheNumber", (new Random().Next(1, 100)));
+            }
+
+            if(result == null)
+            {
+                ViewData["LastGuess"] = "";
+            }
+            else
+            {
+                ViewData["LastGuess"] = result;
+            }
+
+            ViewData["TheNumber"] = HttpContext.Session.GetInt32("TheNumber");
+            return View("index");
+        }
+
+        [HttpPost]
+        public IActionResult guess(int Number)
+        {
+
+            string result;
+            if(Number > HttpContext.Session.GetInt32("TheNumber"))
+            {
+                result = "Too High";
+            }
+            else if(Number < HttpContext.Session.GetInt32("TheNumber"))
+            {
+                result = "Too Low";
+            }
+            else
+            {
+                result = "You Got It!";
+            }
+            
+            return index(result);
+        }
+
+        [HttpGet]
+        public IActionResult reset()
+        {
+            HttpContext.Session.Clear();
+            return index(null);
         }
     }
 }
