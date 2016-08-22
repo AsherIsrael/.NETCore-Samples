3bdf2286932d6e454ce32ca35e18f50acdc0d344
Asher Israel <artificialisrael@yahoo.com>
2016-08-22 10:28:31 -0700

    simple razor with mvc

---
+++ SimpleRazorServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class SimpleController : Controller
+    {
+        public IActionResult index(string name)
+        {
+            ViewData["name"] = name;
+            return View("index");
+        }
+    }
