071c66e647c0b1e16fd75d1c22871e8bb72bb6ac
Asher Israel <artificialisrael@yahoo.com>
2016-08-30 12:15:44 -0700

    wip

---
+++ loginReg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BCrypt.Net;
+
+namespace LoginReg
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
